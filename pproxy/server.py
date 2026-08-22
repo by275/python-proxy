d116cf63322a46b11736f34b5543ab550d504f16
by275 <by275@protonmail.com>
2026-08-22 19:48:42 +0900

    refactor: isolate SSH optional adapter

--- pproxy/server.py
+++ pproxy/server.py
@@ -490,80 +490,6 @@ class ProxyH3(ProxyQUIC):
         import aioquic.asyncio
         return aioquic.asyncio.serve(self.host_name, self.port, configuration=self.quicserver, create_protocol=self.get_protocol(True, functools.partial(stream_handler, **vars(self), **args)))
 
-class ProxySSH(ProxySimple):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        self.sshconn = None
-    def logtext(self, host, port):
-        return f' -> sshtunnel {self.bind}' + self.jump.logtext(host, port)
-    def patch_stream(self, ssh_reader, writer, host, port):
-        reader = asyncio.StreamReader()
-        async def channel():
-            while not ssh_reader.at_eof() and not writer.is_closing():
-                buf = await transport.read(ssh_reader, 65536)
-                if not buf:
-                    break
-                reader.feed_data(buf)
-            reader.feed_eof()
-        create_task(channel())
-        remote_addr = ('ssh:'+str(host), port)
-        writer.get_extra_info = dict(peername=remote_addr, sockname=remote_addr).get
-        return reader, writer
-    async def wait_ssh_connection(self, local_addr=None, family=0, tunnel=None):
-        if self.sshconn is not None and not self.sshconn.cancelled():
-            if not self.sshconn.done():
-                await self.sshconn
-        else:
-            self.sshconn = asyncio.get_running_loop().create_future()
-            try:
-                import asyncssh
-            except Exception:
-                raise Exception('Missing library: "pip3 install asyncssh"')
-            username, password = self.auth.decode().split(':', 1)
-            if password.startswith(':'):
-                client_keys = [password[1:]]
-                password = None
-            else:
-                client_keys = None
-            conn = await asyncssh.connect(host=self.host_name, port=self.port, local_addr=local_addr, family=family, x509_trusted_certs=None, known_hosts=None, username=username, password=password, client_keys=client_keys, keepalive_interval=60, tunnel=tunnel)
-            self.sshconn.set_result(conn)
-    async def wait_open_connection(self, host, port, local_addr, family, tunnel=None):
-        try:
-            await self.wait_ssh_connection(local_addr, family, tunnel)
-            conn = self.sshconn.result()
-            if isinstance(self.jump, ProxySSH):
-                reader, writer = await self.jump.wait_open_connection(host, port, None, None, conn)
-            else:
-                host, port = self.jump.destination(host, port)
-                if self.jump.unix:
-                    reader, writer = await conn.open_unix_connection(self.jump.bind)
-                else:
-                    reader, writer = await conn.open_connection(host, port)
-                reader, writer = self.patch_stream(reader, writer, host, port)
-            return reader, writer
-        except Exception as ex:
-            if not self.sshconn.done():
-                self.sshconn.set_exception(ex)
-            self.sshconn = None
-            raise
-    async def start_server(self, args, stream_handler=stream_handler, tunnel=None):
-        if type(self.jump) is ProxyDirect:
-            raise Exception('ssh server mode unsupported')
-        await self.wait_ssh_connection(tunnel=tunnel)
-        conn = self.sshconn.result()
-        if isinstance(self.jump, ProxySSH):
-            return await self.jump.start_server(args, stream_handler, conn)
-        else:
-            def handler(host, port):
-                def handler_stream(reader, writer):
-                    reader, writer = self.patch_stream(reader, writer, host, port)
-                    return stream_handler(reader, writer, **vars(self.jump), **args)
-                return handler_stream
-            if self.jump.unix:
-                return await conn.start_unix_server(handler, self.jump.bind)
-            else:
-                return await conn.start_server(handler, self.jump.host_name, self.jump.port)
-
 class ProxyBackward(ProxySimple):
     def __init__(self, backward, backward_num, **kw):
         super().__init__(**kw)
@@ -834,6 +760,7 @@ def main(args=None):
 
 
 from .h2 import ProxyH2
+from .ssh import ProxySSH
 
 
 if __name__ == '__main__':
--- tests/test_optional_adapters.py
+++ tests/test_optional_adapters.py
@@ -2,11 +2,15 @@ import unittest
 
 from pproxy import server
 from pproxy.h2 import ProxyH2
+from pproxy.ssh import ProxySSH
 
 
 class OptionalAdapterTests(unittest.TestCase):
     def test_h2_adapter_keeps_server_compatibility_alias(self):
         self.assertIs(server.ProxyH2, ProxyH2)
+
+    def test_ssh_adapter_keeps_server_compatibility_alias(self):
+        self.assertIs(server.ProxySSH, ProxySSH)
 
 
 if __name__ == "__main__":
